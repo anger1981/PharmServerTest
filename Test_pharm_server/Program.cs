19b3fb753009c9e85c8bb50c6598a88d96bde49c
anger1981 <anger1981@rambler.ru>
2017-05-30 22:45:47 +0500

    2

---
+++ Test_pharm_server/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PharmaceuticalInformation.Server;
+
+namespace Test_pharm_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server srv = new Server();
+            srv.StartingOfServer();
+        }
+    }
+}
